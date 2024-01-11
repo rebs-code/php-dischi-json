199c6cb823c4d87f190c70304a7d0ac99f8ebc13
rebs-code <jack.rebussi@gmail.com>
2024-01-11 18:22:45 +0100

    feat: display album title with vue

--- index.php
+++ index.php
@@ -5,13 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Music</title>
+    <!-- my css -->
+    <link rel="stylesheet" href="./src/css/style.css">
 </head>
 
 <body>
+    <div id="app">
+        <header>
+            <h1>Albums</h1>
+        </header>
+        <main>
+            <div v-for="album in albums" v-cloak>
+                <h2>{{album.title}}</h2>
+            </div>
+        </main>
+    </div>
+
     <!-- vue cdn -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!-- axios -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- my JS script -->
+    <script src="./src/js/index.js"></script>
 </body>
 
 </html>
---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      albums: [
+        {
+          cover:
+            "https://albumartexchange.com/coverart/gallery/me/metallica_masterofpuppets_457a.jpg",
+          title: "Master of Puppets",
+          artist: "Metallica",
+          year: 1986,
+        },
+        {
+          cover:
+            "https://albumartexchange.com/coverart/gallery/ac/acdc_backinblack_4rb.jpg",
+          title: "Back in Black",
+          artist: "AC/DC",
+          year: 1980,
+        },
+        {
+          cover:
+            "https://albumartexchange.com/coverart/gallery/ir/ironmaiden_thenumberofthebeast_dkw3.jpg",
+          title: "The Number of the Beast",
+          artist: "Iron Maiden",
+          year: 1982,
+        },
+        {
+          cover:
+            "https://albumartexchange.com/coverart/gallery/bl/blacksabbath_paranoid_6cw.jpg",
+          title: "Paranoid",
+          artist: "Black Sabbath",
+          year: 1970,
+        },
+        {
+          cover:
+            "https://albumartexchange.com/coverart/gallery/gu/gunsnroses_appetitefordestructi_bh3.jpg",
+          title: "Appetite for Destruction",
+          artist: "Guns N' Roses",
+          year: 1987,
+        },
+        {
+          cover:
+            "https://albumartexchange.com/coverart/gallery/me/megadeth_rustinpeace_nit.jpg",
+          title: "Rust in Peace",
+          artist: "Megadeth",
+          year: 1990,
+        },
+      ],
+    };
+  },
+}).mount("#app");
