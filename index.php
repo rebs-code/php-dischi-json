288e9ea59dd4a9e16450fd0b9aadf71a0aac4639
rebs-code <jack.rebussi@gmail.com>
2024-01-11 16:59:44 +0100

    add: vue from cdn to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Music</title>
+</head>
+
+<body>
+    <!-- vue cdn -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</body>
+
+</html>
