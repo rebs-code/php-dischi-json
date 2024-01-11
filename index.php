da2bcfb7115aeb6834ac2bd469bb1bf0cfc3ed09
rebs-code <jack.rebussi@gmail.com>
2024-01-11 18:54:09 +0100

    Add Bootstrap CSS and update album display

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Music</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- my css -->
     <link rel="stylesheet" href="./src/css/style.css">
 </head>
@@ -15,9 +17,15 @@
             <h1>Albums</h1>
         </header>
         <main>
-            <div v-for="album in albums" v-cloak>
-                <h2>{{album.title}}</h2>
+            <div class="container d-flex flex-wrap album-container justify-content-between">
+                <div v-for="album in albums" v-cloak class="card album-card">
+                    <img :src="album.cover" alt="album.title">
+                    <h2>{{album.title}}</h2>
+                    <h4>{{album.artist}}</h4>
+                    <p>{{album.year}}</p>
+                </div>
             </div>
+
         </main>
     </div>
 
